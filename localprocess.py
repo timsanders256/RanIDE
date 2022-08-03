1459b11e9cd4cfdfcdad3a08896c5092b269ae81
timsanders256 <timsanders256@gmail.com>
2022-08-03 09:23:14 +0800

    子进程的类封装

---
+++ localprocess.py
@@ -0,0 +1,22 @@
+from subprocess import Popen,PIPE, STDOUT
+#import subprocess
+import time
+class process():
+    def __init__(self, dir) -> None:
+        self.p = Popen(["python", dir],
+                       stdin=PIPE,
+                       stdout=PIPE,
+                       stderr=PIPE,
+                       encoding='utf-8')
+        
+    def input_str(self, str_in):
+        if(self.p.poll() is not None):
+            return
+        self.p.stdin.write(str_in+'\n')
+        self.p.stdin.flush()
+    
+    def get_output(self):
+        line = self.p.stdout.readline()
+        if(line == '' and self.p.poll() is not None):
+            return ''
+        return line.strip()
