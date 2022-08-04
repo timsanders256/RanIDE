0a7bdd21d93a4e0266fae79b8e091bd4b194f6f9
Leo T <79355500+randytaylor4869@users.noreply.github.com>
2022-08-04 21:56:56 +0800

    添加editor的其他效果

--- app.py
+++ app.py
@@ -8,4 +8,5 @@ def homepage():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    #app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)
