0ef89d74205b04dd8334afee2fd220cf6d5eabd9
wql2002 <70012833+wql2002@users.noreply.github.com>
2022-08-04 19:54:26 +0800

    测试，添加运行按钮功能
    点击“运行”按钮将当前输入内容发送给后端

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, json
+from flask import Flask, jsonify, render_template, request, redirect, url_for, session, json
 import time, random
 app = Flask(__name__)
 app.secret_key = 'komejikoishi'
@@ -6,6 +6,18 @@ app.secret_key = 'komejikoishi'
 @app.route('/') # homepage
 def homepage():
     return render_template('index.html')
+
+@app.route('/runCode', methods=['GET', 'POST'])
+def runCode():
+    res = jsonify({})
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    if request.method == 'POST':
+        # 测试代码（后端可根据自己需要更改），读取前端发送的代码
+        mycode = request.get_json()['code']
+        # mycode格式为str
+        print(type(mycode))
+        print(mycode)     
+    return res
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
