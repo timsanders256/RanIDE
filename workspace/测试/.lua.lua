65b99af76bbc44a8c2c8f1ea2505557e42880837
timsanders256 <timsanders256@gmail.com>
2022-08-24 18:20:28 +0800

    IOdebug

--- workspace/测试/.lua.lua
+++ workspace/测试/.lua.lua
@@ -1,2 +1,8 @@
-io.stdout: setvbuf 'no'local testName = '中文编码输出测试'
+io.stdout: setvbuf 'no' io.stderr: setvbuf 'no' local testName = '中文编码输出测试'
 print(testName)
+
+while true do
+    io.write('-->')
+    local str_1 = io.read()
+    print(str_1)
+end
